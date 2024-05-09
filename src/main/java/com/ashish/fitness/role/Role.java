b4ed01be3c98f27a0c48732d76d38459b76eb89d
Ashish S <ashish@Ashishs-MacBook-Air.local>
2024-05-09 14:52:49 +0530

    add initial files and setup user,role

---
+++ src/main/java/com/ashish/fitness/role/Role.java
@@ -0,0 +1,40 @@
+package com.ashish.fitness.role;
+
+import com.ashish.fitness.user.User;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class Role {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    @Column(unique = true)
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    @JsonIgnore
+    private List<User> users;
+
+
+
+
+
+    @Column(nullable = false,updatable = false)
+    private LocalDateTime createdDate;
+    @LastModifiedDate
+    @Column(insertable = false)
+    private LocalDateTime lastModifiedDate;
+}
